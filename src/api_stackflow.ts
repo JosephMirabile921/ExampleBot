a30bb723953622dcf03d786bfb90c5924c7a3762
Kristin Kim <kristinhajinkim@gmail.com>
2021-04-24 23:40:40 -0400

    function can check if undefined json object or post not answered yet

--- src/api_stackflow.ts
+++ src/api_stackflow.ts
@@ -1,24 +1,38 @@
 const fetch = require("node-fetch"); 
 
-function get_stackflow(method: String, library: String){
+function get_stackflow(method: string, library: string){
 
     //data is sorted based on "votes"
     let api_url="https://api.stackexchange.com/2.2/questions?order=asc&sort=votes&tagged=api%3B" + method + "%3B" + library + "&site=stackoverflow"   
     console.log("\n" , "METHOD: ", method.toUpperCase() ,"\n", "LIBRARY:", library.toUpperCase(), "\n")
-    
+
     getapi(api_url);
     
-    async function getapi(url: String){ 
+    async function getapi(url: string){ 
         
         const response =await fetch(url); //store fetch data in "response"
         var data = await response.json(); //store "response" in form of Json
 
-        // it will print top 5 Q's title
-        for (let i = 0; i < 5; i++){
-            let title = data.items[i].title;
-            console.log("Title",i+1 ,": ", title)
-        }
-    }
-}
+        let i =0;
+        let n =0;
+
+        while(i<5){
+
+            let item = data.items[n];
+            
+            if (item!=null && item.is_answered==true){
+                return item.link;
+                n++;i++;
+            }
+            else if(item.is_answered==false){
+                n++;
+            }
+            else{
+                return 'We are sorry, Stack Deficiency!'
+                break;
+            };
+        };
+    };
+};
 
 export default get_stackflow;
